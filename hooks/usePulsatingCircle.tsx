5cc1c4459333d4b7631176f7dcae8b2cdba4e1ab
MC-THEI <th@mc-kg.de>
2024-09-26 19:36:50 +0200

    feat: Add custom hook for pulsating circle animation

---
+++ hooks/usePulsatingCircle.tsx
@@ -0,0 +1,44 @@
+import { useEffect, useRef, useState } from 'react';
+import { Animated } from 'react-native';
+
+const usePulsatingCircle = (
+  initialRadius = 18,
+  pulseTo = 25,
+  duration = 1000
+) => {
+  const animatedRadius = useRef(new Animated.Value(initialRadius)).current;
+  const [circleRadius, setCircleRadius] = useState(initialRadius);
+
+  const startPulsating = () => {
+    Animated.loop(
+      Animated.sequence([
+        Animated.timing(animatedRadius, {
+          toValue: pulseTo,
+          duration: duration,
+          useNativeDriver: false,
+        }),
+        Animated.timing(animatedRadius, {
+          toValue: initialRadius,
+          duration: duration,
+          useNativeDriver: false,
+        }),
+      ])
+    ).start();
+  };
+
+  useEffect(() => {
+    startPulsating();
+
+    const listenerId = animatedRadius.addListener(({ value }) => {
+      setCircleRadius(value); // Aktualisiere den Zustand mit dem animierten Wert
+    });
+
+    return () => {
+      animatedRadius.removeListener(listenerId); // Bereinigen
+    };
+  }, [animatedRadius, initialRadius, pulseTo, duration]);
+
+  return circleRadius;
+};
+
+export default usePulsatingCircle;
