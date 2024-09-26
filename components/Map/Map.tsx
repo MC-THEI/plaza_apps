c86ab3ddad0f6a05adcf6e00271d22c0cdfb1547
MC-THEI <th@mc-kg.de>
2024-09-26 20:11:01 +0200

    integrate mapbox

--- components/Map/Map.tsx
+++ components/Map/Map.tsx
@@ -1,15 +1,143 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import Mapbox, {
+  Camera,
+  CircleLayer,
+  Images,
+  Image,
+  LocationPuck,
+  MapView,
+  ShapeSource,
+  SymbolLayer,
+} from '@rnmapbox/maps';
 import { GlobalStyles } from '../../constants/styles';
-import Mapbox from '@rnmapbox/maps';
+import { featureCollection, point } from '@turf/helpers';
+import { useAppDispatch, useAppSelector } from '../../store/redux/store';
+import { markers, setCameraPosition } from '../../store/redux/map';
+import usePulsatingCircle from '../../hooks/usePulsatingCircle';
 
 function Map() {
+  const { cameraPosition } = useAppSelector((state) => state.map);
+  const dispatch = useAppDispatch();
+
+  const circleRadius = usePulsatingCircle(17, 20, 1000);
+
   Mapbox.setAccessToken(
-    'pk.eyJ1IjoicGxhemFob3RlbHMiLCJhIjoiY2p6MTI4emx5MGhlbjNna245MTVwdDR0NiJ9.C5wKHhbeIX9mOLUq2Tiutg'
+    'pk.eyJ1IjoicGxhemFob3RlbHMiLCJhIjoiY20xZXRzbXIzMnU2cDJrc2pqbHVuZTF5aSJ9.LGe_kovpnas3HgPRwe8Kew'
   );
+
+  const points = markers.map((marker) => point(marker.coordinates));
 
   return (
     <View style={styles.mapContainer}>
-      <Mapbox.MapView style={styles.map} />
+      <MapView
+        style={styles.map}
+        styleURL={'mapbox://styles/plazahotels/clfflk2kz000j01mq9a6vfisf'}
+        attributionEnabled={false}
+        logoEnabled={false}
+        compassEnabled={false}
+        scaleBarEnabled={false}
+      >
+        <Images>
+          <Image name="topImage">
+            <View
+              style={{
+                width: 16,
+                height: 16,
+                borderRadius: 8,
+                backgroundColor: GlobalStyles.colors.successGreen_primary,
+              }}
+            />
+          </Image>
+        </Images>
+        <Camera
+          zoomLevel={cameraPosition.zoomLevel}
+          centerCoordinate={cameraPosition.centerCoordinate}
+          padding={{
+            paddingBottom: 50,
+            paddingLeft: 50,
+            paddingRight: 50,
+            paddingTop: 50,
+          }}
+        />
+
+        <LocationPuck
+          topImage="topImage"
+          visible={true}
+          scale={['interpolate', ['linear'], ['zoom'], 10, 1.0, 20, 4.0]}
+          pulsing={{
+            isEnabled: true,
+            color: GlobalStyles.colors.successGreen_primary,
+            radius: 40.0,
+          }}
+        />
+
+        <ShapeSource
+          id="objects"
+          cluster={true}
+          clusterRadius={50}
+          shape={featureCollection(points)}
+          onPress={(e) => {
+            const properties = e.features[0].properties;
+            const coords = e.features[0].geometry;
+
+            if (properties.point_count) {
+              dispatch(
+                setCameraPosition({
+                  zoomLevel: 5,
+                  centerCoordinate: [
+                    coords.coordinates[0],
+                    coords.coordinates[1],
+                  ],
+                })
+              );
+            } else {
+              dispatch(
+                setCameraPosition({
+                  zoomLevel: 12,
+                  centerCoordinate: [
+                    coords.coordinates[0],
+                    coords.coordinates[1],
+                  ],
+                })
+              );
+            }
+          }}
+        >
+          <CircleLayer
+            id="clusters"
+            filter={['has', 'point_count']}
+            style={{
+              circleColor: GlobalStyles.colors.accentGold,
+              circleRadius: circleRadius,
+            }}
+          />
+
+          <SymbolLayer
+            id="clusters-count"
+            style={{
+              textField: ['get', 'point_count'], // Dynamische Anzeige der Punktanzahl
+              textSize: 12,
+              textColor: '#000000',
+              textHaloWidth: 1,
+              textPitchAlignment: 'map',
+            }}
+          />
+
+          <SymbolLayer
+            id="hotelIcons"
+            filter={['!', ['has', 'point_count']]}
+            style={{
+              iconImage: 'marker',
+              iconSize: 0.8,
+              iconAnchor: 'bottom',
+            }}
+          />
+
+          <Images
+            images={{ marker: require('../../assets/images/marker.png') }}
+          />
+        </ShapeSource>
+      </MapView>
     </View>
   );
 }
@@ -25,5 +153,6 @@ const styles = StyleSheet.create({
   },
   map: {
     flex: 1,
+    width: '100%',
   },
 });
--- components/Map/MapToolBar.tsx
+++ components/Map/MapToolBar.tsx
@@ -1,14 +1,40 @@
 import { View, StyleSheet } from 'react-native';
 import SearchInput from './SearchInput';
 import MapButton from './MapButton';
+import { useAppDispatch } from '../../store/redux/store';
+import { centerMap, setCameraPosition } from '../../store/redux/map';
+import * as Location from 'expo-location';
 
 function MapToolBar() {
+  const dispatch = useAppDispatch();
+
+  async function handleUserLocation() {
+    let { status } = await Location.requestForegroundPermissionsAsync();
+    if (status !== 'granted') {
+      console.log('Zugriff auf den Standort verweigert');
+      return;
+    }
+
+    let location = await Location.getCurrentPositionAsync({});
+
+    dispatch(
+      setCameraPosition({
+        zoomLevel: 14,
+        centerCoordinate: [location.coords.longitude, location.coords.latitude],
+      })
+    );
+  }
+
+  function handleCenterMap() {
+    dispatch(centerMap());
+  }
+
   return (
     <View style={styles.container}>
       <View style={styles.inputBtnContainer}>
         <SearchInput />
-        <MapButton iconName={'location-map'} />
-        <MapButton iconName={'fullscreen'} />
+        <MapButton iconName={'location-map'} onPress={handleUserLocation} />
+        <MapButton iconName={'fullscreen'} onPress={handleCenterMap} />
       </View>
     </View>
   );
