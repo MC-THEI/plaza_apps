447dd98404360e34c2b6ff49ee8baf991f90c4cc
MC-THEI <th@mc-kg.de>
2024-09-26 20:12:52 +0200

    feat: Add constants for API endpoints and geographical data

---
+++ constants/constants.js
@@ -0,0 +1,38 @@
+export const GERMANY_GEODATA = [10, 51];
+
+export const HOST_URL = 'http://localhost:42342';
+export const ACCESS_TOKEN = '69bae9b7-b731-4603-8625-0e5b783bab60';
+
+export const HOTELS_URL = '?rex-api-call=mc_pwa&type=hotels';
+export const HOTEL_URL = '?rex-api-call=mc_pwa&type=hotel&hotel';
+
+export const OFFERS_URL = '?rex-api-call=mc_pwa&type=packages';
+export const OFFER_URL = '?rex-api-call=mc_pwa&type=package&package';
+
+export const ICON_URL = '?rex-api-call=mc_pwa&type=icons';
+
+export let HOST = 'https://plazahotels.de/';
+//if (window.location.host === 'localhost:8085')
+//  HOST = 'https://preview.plazahotels.de/';
+//  else if (window.location.host === 'pwa-preview.plazahotels.de')
+//  HOST = 'https://preview.plazahotels.de/';
+// else if (window.location.host === 'plazapwa.netlify.app')
+//  HOST = 'https://preview.plazahotels.de/';
+
+export const SEARCHTERMS_OFFERS = [
+  'weekend',
+  'angebot',
+  'bettgeflüster',
+  'genießen',
+  'wandern',
+  'wanndern & genießen',
+  'sommererlebnis',
+  'moonlight',
+];
+export const SEARCHTERMS_HOTELS = [
+  'PLAZA',
+  'INN',
+  'BEST WESTERN',
+  'PLUS',
+  'Premium',
+];
