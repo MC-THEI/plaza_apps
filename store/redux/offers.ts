9ecfc121a4d7cfdb5eab020c6d51f21ce6c07f60
MC-THEI <th@mc-kg.de>
2024-09-27 10:19:29 +0200

    feat: Add setCurrentOffer action to Redux offer slice

--- store/redux/offers.ts
+++ store/redux/offers.ts
@@ -4,13 +4,17 @@ export const offersSlice = createSlice({
   name: 'offers',
   initialState: {
     offers: [],
+    currentOffer: null,
   },
   reducers: {
     addOffers: (state, action) => {
       state.offers = action.payload;
     },
+    setCurrentOffer: (state, action) => {
+      state.currentOffer = action.payload;
+    },
   },
 });
 
 export default offersSlice.reducer;
-export const { addOffers } = offersSlice.actions;
+export const { addOffers, setCurrentOffer } = offersSlice.actions;
