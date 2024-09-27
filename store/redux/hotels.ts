79764dafa6eefd94ab24ed4be6f5bd34b5f392aa
MC-THEI <th@mc-kg.de>
2024-09-27 10:19:10 +0200

    feat: Add setCurrentHotel action to Redux hotels slice

--- store/redux/hotels.ts
+++ store/redux/hotels.ts
@@ -4,13 +4,18 @@ export const hotelsSlice = createSlice({
   name: 'hotels',
   initialState: {
     hotels: [],
+    currentHotel: null,
   },
   reducers: {
     addHotels: (state, action) => {
       state.hotels = action.payload;
     },
+
+    setCurrentHotel: (state, action) => {
+      state.currentHotel = action.payload;
+    },
   },
 });
 
 export default hotelsSlice.reducer;
-export const { addHotels } = hotelsSlice.actions;
+export const { addHotels, setCurrentHotel } = hotelsSlice.actions;
