183aed83298d12add5aabe3a054ddca55fe176e2
MC-THEI <th@mc-kg.de>
2024-09-27 10:18:28 +0200

    feat: Add database integration and data fetching logic to App

--- App.tsx
+++ App.tsx
@@ -11,10 +11,29 @@ import { createDrawerNavigator } from '@react-navigation/drawer';
 import RegistrationScreen from './screens/RegistrationScreen';
 import LoginScreen from './screens/LoginScreen';
 import { NavigationTypes } from './types/NavigationTypes';
-import * as Font from 'expo-font';
 import { useEffect, useState } from 'react';
 import { Text } from 'react-native';
 import FavoritesScreen from './screens/FavoritesScreen';
+import { Provider } from 'react-redux';
+import { store, useAppDispatch } from './store/redux/store';
+import {
+  HOTEL_URL,
+  HOTELS_URL,
+  OFFER_URL,
+  OFFERS_URL,
+} from './constants/constants';
+import useFetchData from './services/getData';
+import { loadFonts } from './utils/helper';
+import { addHotels } from './store/redux/hotels';
+import { addOffers } from './store/redux/offers';
+import { IHotel } from './types/HotelTypes';
+import { IOffer } from './types/OfferTypes';
+import {
+  clearTable,
+  createTable,
+  getDataFromDb,
+  insertData,
+} from './store/database';
 
 const Stack = createNativeStackNavigator();
 const Drawer = createDrawerNavigator();
@@ -102,19 +121,67 @@ function DrawerNavigator() {
   );
 }
 
-export default function App() {
+function Root() {
   const [fontsLoaded, setFontsLoaded] = useState(false);
+  const [dbLoading, setDbLoading] = useState(false);
 
-  const loadFonts = () =>
-    Font.loadAsync({
-      'lato-v16-latin-100': require('./assets/fonts/lato/lato-v16-latin-100.ttf'),
-      'lato-v16-latin-300': require('./assets/fonts/lato/lato-v16-latin-300.ttf'),
-      'lato-v16-latin-700': require('./assets/fonts/lato/lato-v16-latin-700.ttf'),
-      'lato-v16-latin-900': require('./assets/fonts/lato/lato-v16-latin-900.ttf'),
-      'lato-v16-latin-regular': require('./assets/fonts/lato/lato-v16-latin-regular.ttf'),
-      IcoMoon_pwai: require('./assets/fonts/icomoon/pwai/PLAZA-PWA.ttf'),
-      IcoMoon_mci: require('./assets/fonts/icomoon/mci/plazahotels.ttf'),
-    });
+  const dispatch = useAppDispatch();
+
+  const {
+    data: fetchedHotelsData,
+    loading: hotelsLoading,
+    error: hotelsError,
+  } = useFetchData(HOTELS_URL, HOTEL_URL);
+
+  const {
+    data: fetchedOffersData,
+    loading: offersLoading,
+    error: offersError,
+  } = useFetchData(OFFERS_URL, OFFER_URL);
+
+  async function setData() {
+    const hotelsJSON = await getDataFromDb('hotels');
+    const offersJSON = await getDataFromDb('offers');
+
+    if (hotelsJSON && offersJSON) {
+      const hotels = JSON.parse(hotelsJSON[0].hotels);
+      const offers = JSON.parse(offersJSON[0].offers);
+      dispatch(addHotels(hotels));
+      dispatch(addOffers(offers));
+    }
+  }
+
+  useEffect(() => {
+    const init = async () => {
+      if (fetchedHotelsData && fetchedOffersData) {
+        try {
+          // hotels
+          await createTable('hotels');
+          await clearTable('hotels');
+          await insertData(fetchedHotelsData as IHotel[], 'hotels');
+
+          // offers
+          await createTable('offers');
+          await clearTable('offers');
+          await insertData(fetchedOffersData as IOffer[], 'offers');
+
+          setData();
+        } catch (error) {
+          console.error('Error saving new data:', error);
+        }
+      } else if (!fetchedHotelsData && !fetchedOffersData) {
+        setDbLoading(true);
+        try {
+          setData();
+        } catch (error) {
+          console.error(error);
+        } finally {
+          setDbLoading(false);
+        }
+      }
+    };
+    init();
+  }, [fetchedHotelsData, fetchedOffersData]);
 
   useEffect(() => {
     async function loadAllFonts() {
@@ -126,19 +193,42 @@ export default function App() {
       }
     }
     loadAllFonts();
-  }, []);
+  }, [fetchedHotelsData, fetchedOffersData]);
 
-  if (!fontsLoaded) {
-    return <Text>Loading....</Text>;
+  const areHotel = fetchedHotelsData && fetchedHotelsData.length > 0;
+  const areOffers = fetchedOffersData && fetchedOffersData.length > 0;
+
+  if (!fontsLoaded || hotelsLoading || offersLoading || dbLoading) {
+    return (
+      <Text
+        style={{
+          color: 'white',
+          fontSize: 20,
+          textAlign: 'center',
+          fontWeight: 'bold',
+          marginTop: '50%',
+        }}
+      >
+        Loading....
+      </Text>
+    );
   }
 
   return (
+    <NavigationContainer>
+      <HomeStackNavigator />
+      <NavBar />
+    </NavigationContainer>
+  );
+}
+
+export default function App() {
+  return (
     <>
       <StatusBar style="light" />
-      <NavigationContainer>
-        <HomeStackNavigator />
-        <NavBar />
-      </NavigationContainer>
+      <Provider store={store}>
+        <Root />
+      </Provider>
     </>
   );
 }
