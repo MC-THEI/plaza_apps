488f2ca17d65978f502336c525964dff581e0aea
MC-THEI <th@mc-kg.de>
2024-09-29 10:51:13 +0200

    feat: integrate data from redux store

--- components/Home/HotelsSlider.tsx
+++ components/Home/HotelsSlider.tsx
@@ -3,12 +3,16 @@ import SectionTitle from '../../ui/SectionTitle';
 import { hotelCardsTitle } from '../../assets/languages';
 import Cards from '../../ui/Cards';
 import { NavigationTypes } from '../../types/NavigationTypes';
+import { useAppSelector } from '../../store/redux/store';
+import { IHotel } from '../../types/HotelTypes';
 
 function HotelsSlider() {
+  const { hotels } = useAppSelector((state) => state.hotels);
+
   return (
     <SectionWrapper bgColor={'light'}>
       <SectionTitle title={hotelCardsTitle[1]} />
-      <Cards openScreen={NavigationTypes.Hotel} />
+      <Cards cardType={NavigationTypes.Hotel} cardData={hotels as IHotel[]} />
     </SectionWrapper>
   );
 }
--- components/Home/OffersSlider.tsx
+++ components/Home/OffersSlider.tsx
@@ -3,12 +3,16 @@ import SectionTitle from '../../ui/SectionTitle';
 import { offersCardsTitle } from '../../assets/languages';
 import Cards from '../../ui/Cards';
 import { NavigationTypes } from '../../types/NavigationTypes';
+import { useAppSelector } from '../../store/redux/store';
+import { IOffer } from '../../types/OfferTypes';
 
-function OffersSlider() {
+function OffersSlider({}) {
+  const { offers } = useAppSelector((state) => state.offers);
+
   return (
     <SectionWrapper bgColor={'dark'}>
       <SectionTitle title={offersCardsTitle[1]} />
-      <Cards openScreen={NavigationTypes.Offer} />
+      <Cards cardType={NavigationTypes.Offer} cardData={offers as IOffer[]} />
     </SectionWrapper>
   );
 }
