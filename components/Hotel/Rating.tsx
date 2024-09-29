3d332c06d361ae991d51271cdd2aedbff8702bde
MC-THEI <th@mc-kg.de>
2024-09-29 10:54:45 +0200

    feat: dynamically display hotel rating data in Rating component

--- components/Hotel/Rating.tsx
+++ components/Hotel/Rating.tsx
@@ -3,22 +3,30 @@ import SectionTitle from '../../ui/SectionTitle';
 import { ratingTitle } from '../../assets/languages';
 import { Text, View, StyleSheet } from 'react-native';
 import { GlobalStyles } from '../../constants/styles';
+import useHotels from '../../hooks/useHotels';
+import { getCurrentObject } from '../../utils/helper';
+import { IHotel } from '../../types/HotelTypes';
 
 function Rating() {
-  return (
+  const { hotels, currentHotelId } = useHotels();
+  const currentHotel: IHotel = getCurrentObject(hotels, currentHotelId);
+
+  const rating = (
     <SectionWrapper bgColor={'light'}>
       <SectionTitle title={ratingTitle[1]} />
       <View style={styles.ratingContainer}>
         <View style={styles.numberContainer}>
-          <Text style={styles.ratingBumber}>8.4</Text>
+          <Text style={styles.ratingBumber}>{currentHotel?.rating.toTen}</Text>
         </View>
         <View>
-          <Text style={styles.ratingText}>Sehr Gut</Text>
-          <Text>824 Bewertungen auf iiQ</Text>
+          <Text style={styles.ratingText}>{currentHotel?.rating.inWords}</Text>
+          <Text>{currentHotel?.rating.TotalParticipantsText}</Text>
         </View>
       </View>
     </SectionWrapper>
   );
+
+  return currentHotel.rating.TotalParticipants > 0 && rating;
 }
 
 export default Rating;
