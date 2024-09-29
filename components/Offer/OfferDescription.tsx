1e83254bd1670e2418b4d0264a9fb3b31d138237
MC-THEI <th@mc-kg.de>
2024-09-29 10:58:07 +0200

    feat: implement dynamic HTML rendering in OfferDescription
    Integrated WebView to display the current offer's description in HTML format.
    Used useOffers hook to fetch the current offer based on its ID.
    Added an accordionHeight state to manage the height of the accordion dynamically.
    Implemented handleMessage function to receive height updates from the WebView, ensuring proper layout.
    Constructed a dynamic HTML template that includes styles for better presentation, adjusting for the app’s theme.
    Included a script within the HTML to post the height of the document back to React Native for adaptive UI behavior.
    Maintained existing accordion functionality to allow users to toggle the description view.

--- components/Offer/OfferDescription.tsx
+++ components/Offer/OfferDescription.tsx
@@ -1,26 +1,74 @@
 import SectionWrapper from '../../ui/SectionWrapper';
-import { Text, View } from 'react-native';
+import { View, StyleSheet } from 'react-native';
 import SectionTitle from '../../ui/SectionTitle';
 import { ourOfferTitle } from '../../assets/languages';
 import { IcoMoon_pwai } from '../../ui/IcoMoon';
 import { GlobalStyles } from '../../constants/styles';
 import Accordion from '../../ui/Accordion';
 import { useState } from 'react';
+import { WebView } from 'react-native-webview';
+import useOffers from '../../hooks/useOffers';
+import { getCurrentObject } from '../../utils/helper';
 
 function OfferDescription() {
   const [openAccordion, setOpenAccordion] = useState(false);
-  function handleClickOpenAccordion() {
+  const [accordionHeight, setAccordionHeight] = useState(0);
+
+  const { currentOfferId, offers } = useOffers();
+  const currentOffer = getCurrentObject(offers, currentOfferId);
+
+  const handleClickOpenAccordion = () => {
     setOpenAccordion((prev) => !prev);
-  }
+  };
+
+  const handleMessage = (event: any) => {
+    const height = parseFloat(event.nativeEvent.data);
+    if (!isNaN(height)) {
+      setAccordionHeight(height);
+    }
+  };
+
+  const htmlContent = `
+    <!DOCTYPE html>
+    <html>
+      <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <style>
+          body, html {
+          margin: -6px 0 0 0;
+            padding: 0;
+            color: ${GlobalStyles.colors.neutralGray_dark};
+            font-family: "Lato", sans-serif;
+            line-height: 23px;
+          }
+        </style>
+      </head>
+      <body>
+        ${currentOffer?.description || ''}
+        <script>
+          (function() {
+            function sendHeight() {
+              const height = document.body.scrollHeight;
+              window.ReactNativeWebView.postMessage(height);
+            }
+
+            // Send initial height after content is loaded
+            window.onload = sendHeight;
+
+          })();
+        </script>
+      </body>
+    </html>
+  `;
 
   return (
     <SectionWrapper bgColor={'light'}>
-      <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
+      <View style={styles.headerContainer}>
         <SectionTitle title={ourOfferTitle[1]} />
         <IcoMoon_pwai
           name="arrow-back"
           size={20}
-          onPress={() => handleClickOpenAccordion()}
+          onPress={handleClickOpenAccordion}
           style={{
             color: GlobalStyles.colors.accentGold,
             height: 20,
@@ -31,14 +79,27 @@ function OfferDescription() {
       </View>
 
       <Accordion open={openAccordion}>
-        <Text>Lorem ipsum dolor sit amet Lorem</Text>
-        <Text>Lorem ipsum dolor sit amet Lorem</Text>
-        <Text>Lorem ipsum dolor sit amet Lorem</Text>
-        <Text>Lorem ipsum dolor sit amet Lorem</Text>
-        <Text>Lorem ipsum dolor sit amet Lorem</Text>
+        <View style={{ height: accordionHeight }}>
+          <WebView
+            originWhitelist={['*']}
+            source={{ html: htmlContent }}
+            scrollEnabled={false}
+            onMessage={handleMessage}
+          />
+        </View>
       </Accordion>
     </SectionWrapper>
   );
 }
 
 export default OfferDescription;
+
+const styles = StyleSheet.create({
+  headerContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  accordionText: {
+    color: GlobalStyles.colors.neutralGray_dark,
+  },
+});
