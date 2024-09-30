a7f83d421857fba31b2c5d891c9025a2e3959a40
MC-THEI <th@mc-kg.de>
2024-09-30 07:33:45 +0200

    fix: Add conditional rendering for Information section

--- components/Hotel/Information.tsx
+++ components/Hotel/Information.tsx
@@ -2,14 +2,25 @@ import SectionWrapper from '../../ui/SectionWrapper';
 import SectionTitle from '../../ui/SectionTitle';
 import { informationTitle } from '../../assets/languages';
 import InformationCards from './InformationCards';
+import useHotels from '../../hooks/useHotels';
+import { IHotel } from '../../types/HotelTypes';
+import { getCurrentObject } from '../../utils/helper';
 
 function Information() {
-  return (
-    <SectionWrapper bgColor={'dark'}>
+  const { hotels, currentHotelId } = useHotels();
+  const currentHotel: IHotel = getCurrentObject(hotels, currentHotelId);
+
+  const props = Object.values(currentHotel.properties);
+  console.log(props);
+
+  const information = (
+    <SectionWrapper>
       <SectionTitle title={informationTitle[1]} />
       <InformationCards />
     </SectionWrapper>
   );
+
+  return props.length > 0 && information;
 }
 
 export default Information;
