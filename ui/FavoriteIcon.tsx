788b2481ce4ee80c0a14cc619b842ce0541c4501
MC-THEI <th@mc-kg.de>
2024-10-01 07:01:43 +0200

    feat: enhance FavoriteIcon with Redux integration for dynamic favorites
     Refactored `FavoriteIcon` component to integrate with Redux for managing favorites:
      - Removed local state management in favor of Redux state, enhancing consistency across the app.
      - Introduced props (`id` and `favName`) to enable the icon to handle both hotel and offer favorites dynamically.
    
    - Added logic to determine if the item is a favorite:
      - Utilized `useAppSelector` to retrieve favorite IDs from the Redux store.
      - Implemented a toggle function (`toggleFavorite`) that updates the favorite status based on the item type.
    
    - Updated rendering logic:
      - The favorite icon now updates dynamically based on its favorite status, improving user interaction.
      - Ensured the `Pressable` component triggers the `toggleFavorite` function on press, enhancing user experience.
    
    These changes improve the `FavoriteIcon` component by making it more versatile and allowing for consistent state management across the application.

--- ui/FavoriteIcon.tsx
+++ ui/FavoriteIcon.tsx
@@ -1,13 +1,44 @@
 import { GlobalStyles } from '../constants/styles';
 import { IcoMoon_pwai } from './IcoMoon';
-import { useState } from 'react';
 import { Pressable } from 'react-native';
+import { useAppDispatch, useAppSelector } from '../store/redux/store';
+import {
+  storeDataAsync,
+  toggleFavoriteHotel,
+  toggleFavoriteOffer,
+} from '../store/redux/favorites';
 
-function FavoriteIcon({ size }: { size?: number }) {
-  const [isFavorite, setIsFavorite] = useState(true);
+function FavoriteIcon({
+  size,
+  id,
+  favName,
+}: {
+  size?: number;
+  id: number;
+  favName: string;
+}) {
+  const { favoritesHotelIds, favoritesOfferIds } = useAppSelector(
+    (state) => state.favorites
+  );
+
+  const dispatch = useAppDispatch();
+
+  const isHotelFav = favName === 'Hotel';
+  const favoriteIds = isHotelFav ? favoritesHotelIds : favoritesOfferIds;
+  const isFavorite = favoriteIds.indexOf(id) !== -1;
+
+  function toggleFavorite() {
+    if (isHotelFav) {
+      dispatch(toggleFavoriteHotel(id as number));
+      dispatch(storeDataAsync());
+    } else {
+      dispatch(toggleFavoriteOffer(id as number));
+      dispatch(storeDataAsync());
+    }
+  }
 
   return (
-    <Pressable>
+    <Pressable onPress={() => toggleFavorite()}>
       {isFavorite && (
         <IcoMoon_pwai
           name="heart"
