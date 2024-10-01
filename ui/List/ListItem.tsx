0744191089b19eec0690a82b272082481d48959b
MC-THEI <th@mc-kg.de>
2024-10-01 06:59:22 +0200

    refactor: extend ListItem to support hotels and offers, update styles and remove dummy data
    Refactored `ListItem` to support both `IHotel` and `IOffer` types.
      - Added new props (`item`, `bgColor`, `listType`) to make `ListItem` more versatile.
      - Removed dummy data and used dynamic item props instead.
      - Integrated helper functions (`getCountryCode`, `getCurrentObject`, etc.) for better data handling.
      - Added `useHotels` hook to fetch hotel details when needed.
    
    - Updated image source handling:
      - Replaced the static image URL with `getFullImageUrl(item.titlePicture)` to provide dynamic images.
    
    - Added new style elements to improve UI:
      - Introduced `iconContainer` and `iconContainerOffer` to handle different styles for icons based on list type.
      - Updated `container` style to include `position: relative` for better icon positioning.
      - Styled the offer icon with `successGreen_primary` to visually differentiate offers.
    
    This refactor makes `ListItem` more modular and flexible, enabling it to handle different data types effectively while improving maintainability and UI clarity.

--- ui/List/ListItem.tsx
+++ ui/List/ListItem.tsx
@@ -2,15 +2,44 @@ import { Image, View, StyleSheet, Pressable, Text } from 'react-native';
 import { GlobalStyles } from '../../constants/styles';
 import { IcoMoon_mci, IcoMoon_pwai } from '../IcoMoon';
 import FavoriteIcon from '../FavoriteIcon';
+import { IHotel } from '../../types/HotelTypes';
+import { IOffer } from '../../types/OfferTypes';
+import {
+  getCountryCode,
+  getCurrentObject,
+  getFullImageUrl,
+} from '../../utils/helper';
+import useHotels from '../../hooks/useHotels';
 
-const dummyData = {
-  id: 1,
-  imgUrl: 'https://preview.plazahotels.de/media/210_dz-komfort.jpg',
-  name: 'Plaza Hotels Hanauu',
-  location: 'DE-Brunswick',
-};
+function ListItem({
+  item,
+  bgColor,
+  listType,
+}: {
+  item: IHotel | IOffer;
+  bgColor: string;
+  listType: 'hotel' | 'offer';
+}) {
+  const { hotels } = useHotels();
 
-function ListItem({ item, bgColor }: { item: any; bgColor: string }) {
+  const iconLogo =
+    listType === 'hotel' ? (
+      <Image source={{ uri: 'https://placehold.co/50.png' }} />
+    ) : (
+      <IcoMoon_pwai name={'gift'} color={'white'} size={16} />
+    );
+
+  const offerHotel =
+    listType === 'offer' && getCurrentObject(hotels, item.hotel?.id);
+
+  const city =
+    listType === 'hotel' ? item.location?.city : offerHotel.location?.city;
+
+  const country =
+    listType === 'hotel'
+      ? item.location?.country
+      : offerHotel.location?.country;
+
   return (
     <Pressable
       style={[
@@ -21,34 +50,47 @@ function ListItem({ item, bgColor }: { item: any; bgColor: string }) {
         },
       ]}
     >
+      <View
+        style={[
+          styles.iconContainer,
+          listType === 'offer' && styles.iconContainerOffer,
+        ]}
+      >
+        {iconLogo}
+      </View>
       <View style={styles.imgTextContainer}>
         <View style={styles.imageContainer}>
           <Image
             style={styles.image}
             source={{
-              uri: dummyData.imgUrl,
+              uri: getFullImageUrl(item.titlePicture),
             }}
             resizeMode="cover"
           />
         </View>
         <View style={styles.textContainer}>
           <View>
-            <Text style={styles.textName}>{dummyData.name}</Text>
+            <Text style={styles.textName}>{item.name}</Text>
             <View style={styles.locationContainer}>
               <IcoMoon_mci
                 name="marker"
                 color={GlobalStyles.colors.accentGold}
                 size={15}
               />
-              <Text style={styles.locationText}>{dummyData.location}</Text>
+              <Text style={styles.locationText}>
+                {getCountryCode(country)}
+                {city}
+              </Text>
             </View>
           </View>
-          {/* Name*/}
-          {/* Location*/}
         </View>
       </View>
       <View style={styles.favBtnContainer}>
-        <FavoriteIcon size={20} />
+        <FavoriteIcon
+          size={20}
+          id={item.id}
+          favName={listType === 'hotel' ? 'Hotel' : 'Offer'}
+        />
       </View>
     </Pressable>
   );
@@ -58,10 +100,28 @@ export default ListItem;
 
 const styles = StyleSheet.create({
   container: {
+    position: 'relative',
     height: 87,
-    width: '100%',
     flexDirection: 'row',
     justifyContent: 'space-between',
+  },
+  iconContainer: {
+    position: 'absolute',
+    top: 0,
+    left: 15,
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+
+    width: 30,
+    height: 26,
+    backgroundColor: GlobalStyles.colors.neutralWhite,
+    zIndex: 1,
+    borderBottomRightRadius: 6,
+    borderBottomLeftRadius: 6,
+  },
+  iconContainerOffer: {
+    backgroundColor: GlobalStyles.colors.successGreen_primary,
   },
   imgTextContainer: {
     flexDirection: 'row',
@@ -70,6 +130,7 @@ const styles = StyleSheet.create({
     width: 120,
     height: '100%',
   },
+
   image: {
     width: '100%',
     height: '97%',
