f9958455f94bc795bdbe596503324a2b3651d4ec
MC-THEI <th@mc-kg.de>
2024-10-01 06:57:01 +0200

    feat: Enhance favorites slice with async storage and new toggling functionality
     Added `storeDataAsync` and `getDataAsync` async thunks for storing and retrieving favorite data from AsyncStorage.
      - `storeDataAsync`: Stores `favoritesHotelIds` and `favoritesOfferIds` in AsyncStorage, ensuring persistence across sessions.
      - `getDataAsync`: Retrieves these values from AsyncStorage on initialization.
    
    - Updated initial state:
      - Replaced `favoritesHotels` and `favoritesOffers` with `favoritesHotelIds` and `favoritesOfferIds` for better state management.
      - Added `favoritesLoading` to manage loading state during async operations.
      - Added `hotelsBtnActivated` and `offersBtnActivated` to manage filter button states for hotel and offer favorites.
    
    - Modified actions:
      - Replaced separate `addFavoriteHotel`, `removeFavoriteHotel`, `addFavoriteOffer`, and `removeFavoriteOffer` actions with unified `toggleFavoriteHotel` and `toggleFavoriteOffer` actions.
        - `toggleFavoriteHotel`/`toggleFavoriteOffer`: Add or remove the hotel/offer ID from the favorite list based on its current presence.
    
    - Added filter toggle actions:
      - `toggleHotelsFilter` and `toggleOffersFilter` allow users to toggle the active state of hotel and offer buttons.
    
    - Integrated extra reducers to handle async actions:
      - Managed pending, fulfilled, and rejected states for both `storeDataAsync` and `getDataAsync` to keep track of loading state and error handling.
    
    This commit enhances the favorites feature by allowing persistent storage, simplifying reducer logic, and adding toggling capabilities for more dynamic user interactions.

--- store/redux/favorites.ts
+++ store/redux/favorites.ts
@@ -1,35 +1,124 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createAsyncThunk, createSlice, PayloadAction } from '@reduxjs/toolkit';
+import {
+  clearDataFromAsyncStorage,
+  getMultipleDataFromAsyncStorage,
+  storeMultipleDataInAsyncStorage,
+} from '../../services/setDataToAsynStorage';
+
+export const storeDataAsync = createAsyncThunk(
+  'favorites/setDataInAsyncStorage',
+  async (_, { getState }) => {
+    const state = getState();
+    const { favoritesHotelIds, favoritesOfferIds } = state.favorites;
+
+    await clearDataFromAsyncStorage('favoritesHotelIds');
+    await clearDataFromAsyncStorage('favoritesOfferIds');
+
+    await storeMultipleDataInAsyncStorage(
+      'favoritesHotelIds',
+      'favoritesOfferIds',
+      favoritesHotelIds,
+      favoritesOfferIds
+    );
+  }
+);
+
+export const getDataAsync = createAsyncThunk(
+  'favorites/getDataFromAsyncStorage',
+  async () => {
+    const keys = ['favoritesHotelIds', 'favoritesOfferIds'];
+    const data = await getMultipleDataFromAsyncStorage(keys);
+
+    if (!data) {
+      return [[], []];
+    }
+
+    const favorites = data.map(([, value]) => value ?? []);
+    return favorites;
+  }
+);
 
 const favoritesSlice = createSlice({
   name: 'favorites',
   initialState: {
-    favoritesHotels: [],
-    favoritesOffers: [],
+    favoritesHotelIds: [],
+    favoritesOfferIds: [],
+    favoritesLoading: false,
+    hotelsBtnActivated: true,
+    offersBtnActivated: true,
   },
   reducers: {
-    addFavoriteHotel: (state, action) => {
-      state.favoritesHotels.push(action.payload);
-    },
-    removeFavoriteHotel: (state, action) => {
-      state.favoritesHotels = state.favoritesHotels.filter(
-        (favorite) => favorite.id !== action.payload
+    toggleFavoriteHotel: (state, action: PayloadAction<number>) => {
+      const hotelIndex = state.favoritesHotelIds.findIndex(
+        (favoriteId: number) => favoriteId === (action.payload as number)
       );
+
+      if (hotelIndex !== -1) {
+        state.favoritesHotelIds.splice(hotelIndex, 1);
+      } else {
+        state.favoritesHotelIds.push(action.payload);
+      }
     },
-    addFavoriteOffer: (state, action) => {
-      state.favoritesOffers.push(action.payload);
-    },
-    removeFavoriteOffer: (state, action) => {
-      state.favoritesOffers = state.favoritesOffers.filter(
-        (favorite) => favorite.id !== action.payload
+
+    toggleFavoriteOffer: (state, action: PayloadAction<number>) => {
+      const offerIndex = state.favoritesOfferIds.findIndex(
+        (favoriteId) => favoriteId === (action.payload as number)
       );
+
+      if (offerIndex !== -1) {
+        state.favoritesOfferIds.splice(offerIndex, 1);
+      } else {
+        state.favoritesOfferIds.push(action.payload);
+      }
     },
+    toggleHotelsFilter: (state) => {
+      state.hotelsBtnActivated = true;
+      state.offersBtnActivated = !state.offersBtnActivated;
+    },
+
+    toggleOffersFilter: (state) => {
+      state.offersBtnActivated = true;
+      state.hotelsBtnActivated = !state.hotelsBtnActivated;
+    },
+  },
+  extraReducers: (builder) => {
+    builder.addCase(storeDataAsync.pending, (state, action) => {
+      state.favoritesLoading = true;
+    });
+    builder.addCase(storeDataAsync.fulfilled, (state, action) => {
+      state.favoritesLoading = false;
+    });
+    builder.addCase(storeDataAsync.rejected, (state, action) => {
+      state.favoritesLoading = false;
+      console.error('getData rejected:', action.error);
+    });
+
+    builder.addCase(getDataAsync.pending, (state, action) => {
+      state.favoritesLoading = true;
+    });
+    builder.addCase(getDataAsync.fulfilled, (state, action) => {
+      console.log('action.payload', action.payload);
+      if (action.payload) {
+        state.favoritesLoading = false;
+        state.favoritesHotelIds = action.payload[0];
+        state.favoritesOfferIds = action.payload[1];
+      } else {
+        console.error('No payload received in getDataAsync.fulfilled');
+      }
+    });
+    builder.addCase(getDataAsync.rejected, (state, action) => {
+      state.favoritesLoading = false;
+      console.error('getData rejected:', action.error);
+    });
   },
 });
 
-export const addFavoriteHotel = favoritesSlice.actions.addFavoriteHotel;
-export const addFavoriteOffer = favoritesSlice.actions.addFavoriteOffer;
+export const toggleFavoriteHotel = favoritesSlice.actions.toggleFavoriteHotel;
 
-export const removeFavoriteHotel = favoritesSlice.actions.removeFavoriteHotel;
-export const removeFavoriteOffer = favoritesSlice.actions.removeFavoriteOffer;
+export const toggleFavoriteOffer = favoritesSlice.actions.toggleFavoriteOffer;
+
+export const toggleHotelsFilter = favoritesSlice.actions.toggleHotelsFilter;
+
+export const toggleOffersFilter = favoritesSlice.actions.toggleOffersFilter;
 
 export default favoritesSlice.reducer;
