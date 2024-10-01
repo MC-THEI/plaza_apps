ec6eb5196e0b6eceb7013d937ef2d3658c9460ab
MC-THEI <th@mc-kg.de>
2024-10-01 06:52:37 +0200

    feat(favorites): Dynamically display favorite hotels and offers
    Integrated `useFavorites`, `useHotels`, and `useOffers` hooks to manage state and filter data.
    - Implemented dynamic rendering of favorite hotels and offers using `filterObjectsByIds`.
    - Conditionally display favorites based on whether they are available and which buttons are activated.
    - Enhanced FavoritesList component by adding proper data passing to `ObjectList` for better functionality.

--- components/Favorites/FavoritesList.tsx
+++ components/Favorites/FavoritesList.tsx
@@ -5,20 +5,46 @@ import {
   mainTitleHotelFavorites,
   mainTitleOfferFavorites,
 } from '../../assets/languages';
+import useFavorites from '../../hooks/useFavorites';
+import useHotels from '../../hooks/useHotels';
+import useOffers from '../../hooks/useOffers';
+import { filterObjectsByIds } from '../../utils/helper';
+import { IOffer } from '../../types/OfferTypes';
+import { IHotel } from '../../types/HotelTypes';
 
 function FavoritesList() {
+  const {
+    favoritesHotelIds,
+    favoritesOfferIds,
+    hotelsBtnActivated,
+    offersBtnActivated,
+  } = useFavorites();
+  const { hotels } = useHotels();
+  const { offers } = useOffers();
+
+  const favHotels = filterObjectsByIds(hotels, favoritesHotelIds);
+  const favOffers = filterObjectsByIds(offers, favoritesOfferIds);
+
+  const hotelsList = (
+    <ObjectList data={favHotels as IHotel[]} listType="hotel">
+      <View style={styles.title}>
+        <SectionTitle title={mainTitleHotelFavorites[1]} />
+      </View>
+    </ObjectList>
+  );
+
+  const offersList = (
+    <ObjectList data={favOffers as IOffer[]} listType="offer">
+      <View style={styles.title}>
+        <SectionTitle title={mainTitleOfferFavorites[1]} />
+      </View>
+    </ObjectList>
+  );
+
   return (
     <View style={styles.listContainer}>
-      <ObjectList>
-        <View style={styles.title}>
-          <SectionTitle title={mainTitleHotelFavorites[1]} />
-        </View>
-      </ObjectList>
-      <ObjectList>
-        <View style={styles.title}>
-          <SectionTitle title={mainTitleOfferFavorites[1]} />
-        </View>
-      </ObjectList>
+      {favHotels.length > 0 && hotelsBtnActivated && hotelsList}
+      {favOffers.length > 0 && offersBtnActivated && offersList}
     </View>
   );
 }
--- components/Favorites/FilterButtons.tsx
+++ components/Favorites/FilterButtons.tsx
@@ -1,13 +1,39 @@
 import { View, StyleSheet } from 'react-native';
 import MainButton from '../../ui/MainButton';
 import { filterButtonsTitle } from '../../assets/languages';
+import {
+  toggleHotelsFilter,
+  toggleOffersFilter,
+} from '../../store/redux/favorites';
+import { useAppDispatch } from '../../store/redux/store';
+import useFavorites from '../../hooks/useFavorites';
 
 function FilterButtons() {
+  const dispatch = useAppDispatch();
+
+  const { hotelsBtnActivated, offersBtnActivated } = useFavorites();
+
+  function handleClickHotelsFilterBtn() {
+    dispatch(toggleHotelsFilter());
+  }
+
+  function handleClickOffersFilterBtn() {
+    dispatch(toggleOffersFilter());
+  }
+
   return (
     <View style={styles.buttonOuterContainer}>
       <View style={styles.buttonInnerContainer}>
-        <MainButton title={filterButtonsTitle.hotels[1]} />
-        <MainButton title={filterButtonsTitle.offers[1]} />
+        <MainButton
+          inverted={!hotelsBtnActivated}
+          title={filterButtonsTitle.hotels[1]}
+          onPress={handleClickHotelsFilterBtn}
+        />
+        <MainButton
+          inverted={!offersBtnActivated}
+          title={filterButtonsTitle.offers[1]}
+          onPress={handleClickOffersFilterBtn}
+        />
       </View>
     </View>
   );
