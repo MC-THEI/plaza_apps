811cf4592c36de7136e97cc3df535ef29cf634c3
MC-THEI <th@mc-kg.de>
2024-10-06 17:08:13 +0200

    feat: add navigation to SearchButtons for map screen transitions
    - Updated `SearchButtons` component to navigate to the map screen when buttons are pressed.
    - Added `useNavigation` to facilitate navigation to the Map screen (`NavigationTypes.Map`).
    - Introduced `handlePress` function to handle button clicks for both Hotels and Offers, dispatching `setSelectedMapList` to Redux.
    - Integrated `useAppDispatch` to set the selected map list type (Hotels or Offers) before navigating.
    - Modified `MainButton` components to include `onPress` handlers for transitioning to the correct map view based on the selection.

--- components/Home/SearchButtons.tsx
+++ components/Home/SearchButtons.tsx
@@ -1,13 +1,31 @@
 import { View, StyleSheet } from 'react-native';
-import MainButton from '../../ui/MainButton';
+import { useNavigation } from '@react-navigation/native';
+import { useAppDispatch } from '../../store/redux/store';
 import { searchButtonsTitle } from '../../assets/languages';
+import { setSelectedMapList } from '../../store/redux/map';
+import { NavigationTypes } from '../../types/NavigationTypes';
+import MainButton from '../../ui/MainButton';
 
 function SearchButtons() {
+  const dispatch = useAppDispatch();
+  const navigation = useNavigation();
+
+  function handlePress(type: NavigationTypes) {
+    dispatch(setSelectedMapList(type));
+    (navigation.navigate as (routeName: string) => void)(NavigationTypes.Map);
+  }
+
   return (
     <View style={styles.buttonOuterContainer}>
       <View style={styles.buttonInnerContainer}>
-        <MainButton title={searchButtonsTitle.hotels[1]} />
-        <MainButton title={searchButtonsTitle.offers[1]} />
+        <MainButton
+          title={searchButtonsTitle.hotels[1]}
+          onPress={() => handlePress(NavigationTypes.Hotel)}
+        />
+        <MainButton
+          title={searchButtonsTitle.offers[1]}
+          onPress={() => handlePress(NavigationTypes.Offer)}
+        />
       </View>
     </View>
   );
