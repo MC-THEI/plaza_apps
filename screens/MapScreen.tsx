13dc3ae95f2076d28c70374011017c215fdfe348
MC-THEI <th@mc-kg.de>
2024-10-06 17:14:16 +0200

    fix(MapScreen): Initialize map state and add background color
    - Dispatch setMapChanging, resetMapStates, and getObjectsThunk on component mount to initialize map state.
    - Wrap Map, MapToolBar, and MapList components in a View with a white background to enhance visual clarity.

--- screens/MapScreen.tsx
+++ screens/MapScreen.tsx
@@ -2,14 +2,29 @@ import { StatusBar } from 'expo-status-bar';
 import MapList from '../components/Map/MapList';
 import Map from '../components/Map/Map';
 import MapToolBar from '../components/Map/MapToolBar';
+import { useAppDispatch } from '../store/redux/store';
+import {
+  getObjectsThunk,
+  resetMapStates,
+  setMapChanging,
+} from '../store/redux/map';
+import { View } from 'react-native';
 
 function MapScreen() {
+  const dispatch = useAppDispatch();
+
+  dispatch(setMapChanging(true));
+  dispatch(resetMapStates());
+  dispatch(getObjectsThunk());
+
   return (
     <>
       <StatusBar style="dark" />
-      <Map />
-      <MapToolBar />
-      <MapList />
+      <View style={{ backgroundColor: 'white' }}>
+        <Map />
+        <MapToolBar />
+        <MapList />
+      </View>
     </>
   );
 }
