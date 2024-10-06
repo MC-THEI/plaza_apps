0821d6275ac816133c9f2d47167de7fa5a61583e
MC-THEI <th@mc-kg.de>
2024-10-06 17:56:05 +0200

    feat(Map): Refactor MapToolBar for improved user location handling
    - Removed inline user location handling and replaced it with a custom hook (useUserLocation).
    - Integrated loading state for user location button to enhance user feedback.
    - Dispatch updated actions to center the map and fetch objects upon user location retrieval.
    - Improved code readability and maintainability by organizing the location logic into a dedicated hook.

--- components/Map/MapToolBar.tsx
+++ components/Map/MapToolBar.tsx
@@ -2,38 +2,33 @@ import { View, StyleSheet } from 'react-native';
 import SearchInput from './SearchInput';
 import MapButton from './MapButton';
 import { useAppDispatch } from '../../store/redux/store';
-import { centerMap, setCameraPosition } from '../../store/redux/map';
-import * as Location from 'expo-location';
+import {
+  centerMap,
+  getObjectsThunk,
+  setMapChanging,
+} from '../../store/redux/map';
+
+import useUserLocation from '../../hooks/mapHooks/useUserLocation';
 
 function MapToolBar() {
   const dispatch = useAppDispatch();
-
-  async function handleUserLocation() {
-    let { status } = await Location.requestForegroundPermissionsAsync();
-    if (status !== 'granted') {
-      console.log('Zugriff auf den Standort verweigert');
-      return;
-    }
-
-    let location = await Location.getCurrentPositionAsync({});
-
-    dispatch(
-      setCameraPosition({
-        zoomLevel: 14,
-        centerCoordinate: [location.coords.longitude, location.coords.latitude],
-      })
-    );
-  }
+  const { isLoadingUserLocation, handleUserLocation } = useUserLocation();
 
   function handleCenterMap() {
     dispatch(centerMap());
+    dispatch(getObjectsThunk());
+    dispatch(setMapChanging(true));
   }
 
   return (
     <View style={styles.container}>
       <View style={styles.inputBtnContainer}>
         <SearchInput />
-        <MapButton iconName={'location-map'} onPress={handleUserLocation} />
+        <MapButton
+          iconName={isLoadingUserLocation ? 'loading' : 'location-map'}
+          onPress={handleUserLocation}
+          isLoading={isLoadingUserLocation}
+        />
         <MapButton iconName={'fullscreen'} onPress={handleCenterMap} />
       </View>
     </View>
