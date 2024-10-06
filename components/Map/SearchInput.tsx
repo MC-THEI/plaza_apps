04cddfc4847d335423610355c80ef995a3dfa349
MC-THEI <th@mc-kg.de>
2024-10-06 17:55:06 +0200

    feat: Enhance SearchInput with debounce and close functionality
    - Implemented debounce mechanism for search input to reduce unnecessary dispatch calls.
    - Added close button functionality to clear the search input and reset the map state.
    - Integrated Redux actions to manage search input value and map state updates.
    - Enhanced styles for better user experience and positioning of the close button.

--- components/Map/SearchInput.tsx
+++ components/Map/SearchInput.tsx
@@ -1,23 +1,84 @@
-import { TextInput, StyleSheet } from 'react-native';
+import { TextInput, StyleSheet, View, Text, Pressable } from 'react-native';
 import { GlobalStyles } from '../../constants/styles';
-import { useState } from 'react';
+import React, { useState } from 'react';
+import { searchInputPlaceholder } from '../../assets/languages';
+import { useAppDispatch } from '../../store/redux/store';
+import {
+  centerMap,
+  getObjectsThunk,
+  setMapChanging,
+  setSearchInputValue,
+} from '../../store/redux/map';
+import { IcoMoon_mci, IcoMoon_pwai } from '../../ui/IcoMoon';
 
 function SearchButton() {
-  const [input, setInput] = useState();
+  const [input, setInput] = useState('');
+  const [debounceTimeout, setDebounceTimeout] = useState<number | null>(null);
+  const dispatch = useAppDispatch();
 
-  return <TextInput style={styles.input} placeholder={'Hotel suchen ...'} />;
+  const handleSearchInput = (value: string) => {
+    setInput(value);
+
+    if (debounceTimeout) {
+      clearTimeout(debounceTimeout);
+    }
+
+    const newTimeout = setTimeout(() => {
+      dispatch(setSearchInputValue(value));
+      dispatch(getObjectsThunk());
+      dispatch(setMapChanging(true));
+    }, 800);
+    setDebounceTimeout(newTimeout as unknown as number);
+  };
+
+  function handlePressCLoseBtn() {
+    setInput('');
+    dispatch(setSearchInputValue(''));
+    dispatch(getObjectsThunk());
+    dispatch(centerMap());
+  }
+
+  const closeButton = (
+    <Pressable
+      style={styles.iconContainer}
+      onPress={() => handlePressCLoseBtn()}
+    >
+      <IcoMoon_mci name="close" size={20} />
+    </Pressable>
+  );
+
+  return (
+    <View style={styles.container}>
+      <TextInput
+        style={styles.input}
+        value={input}
+        onChangeText={handleSearchInput}
+        placeholder={searchInputPlaceholder[1]}
+      />
+      {input !== '' && closeButton}
+    </View>
+  );
 }
 
 export default SearchButton;
 
 const styles = StyleSheet.create({
+  container: {
+    width: '60%',
+    position: 'relative',
+  },
   input: {
     height: 50,
-    width: '60%',
+    paddingRight: 45,
     paddingHorizontal: 20,
     backgroundColor: GlobalStyles.colors.accentGold,
     fontFamily: 'lato-v16-latin-700',
     fontSize: GlobalStyles.fontSize.inputFontSize,
     borderRadius: 8,
+  },
+  iconContainer: {
+    position: 'absolute',
+    right: 15,
+    top: 15,
   },
 });
